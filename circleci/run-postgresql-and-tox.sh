cf27145d1dade22a9cc035137b5e6c85c5f00302
gregn <gregn2017@gregn.fastmail.com>
2017-11-25 16:45:43 +0100

    doctest README.adoc build fix

--- circleci/run-postgresql-and-tox.sh
+++ circleci/run-postgresql-and-tox.sh
@@ -3,6 +3,9 @@
 ./pgsql-9.3.13/bin/postgres -D `pwd`/pgsql-9.3.13/data -p 5493 &> pgsql-9.3.log &
 ./pgsql-9.4.8/bin/postgres -D `pwd`/pgsql-9.4.8/data -p 5494 &> pgsql-9.4.log &
 ./pgsql-9.5.3/bin/postgres -D `pwd`/pgsql-9.5.3/data -p 5495 &> pgsql-9.5.log &
+
+# Run another instance on default port for doctest README.adoc
+./pgsql-9.5.3/bin/initdb -D `pwd`/pgsql-9.5.3/data2
 ./pgsql-9.5.3/bin/postgres -D `pwd`/pgsql-9.5.3/data2 -p 5432 &> pgsql-9.5-data2.log &
 
 cd /home/postgres/pg8000-src/
